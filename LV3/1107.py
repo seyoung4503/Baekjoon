f0bb2dc9d7b1ee88893bdd5f0d39a66bc6454780
seyoung4503 <seyoung4503@gmail.com>
2024-03-13 23:42:18 +0900

    50

--- LV3/1107.py
+++ LV3/1107.py
@@ -4,11 +4,21 @@ input = sys.stdin.readline
 channel_n = int(input())
 
 broken_button_n = int(input())
-
-broken_buttons = list(map(int, input().split()))
+broken_buttons = []
+if broken_button_n != 0:
+    broken_buttons = list(map(int, input().split()))
+# print(broken_buttons)
 
 maximum_pressed = abs(channel_n - 100)
 
-nums = [(channel_n % (10^i)) for i in range(len(str(channel_n))+1, 1)]
+nums_min = maximum_pressed
 
-print(nums)
+# count = 0
+for i in range(1000001):
+    for j in range(len(str(i))):
+        if int(str(i)[j]) in broken_buttons:
+            break
+        elif j == len(str(i)) - 1:
+            nums_min = min(len(str(i)) + abs(i - channel_n), nums_min)
+
+print(nums_min)
