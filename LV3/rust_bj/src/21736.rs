d2d6c887d025aab9d724ae35c136b26f065095cf
최세영 <seyoung4503@gmail.com>
2024-05-16 21:48:09 +0900

    21736 1hour

--- LV3/rust_bj/src/21736.rs
+++ LV3/rust_bj/src/21736.rs
@@ -5,41 +5,68 @@ use std::any::type_name_of_val;
 
 
 
-fn bfs() {
+fn bfs(let start_position) {
+    let mut mat: Vec<Vec<usize>> = Vec::new();
 
+    mat.push(start_position);
+    
+    for i in 0..4 {
+
+    }
 }
 
 
 fn main() {
     let mut buffer = String::new();
-    stdin().read_to_string(&mut buffer).unwrap();
+    stdin().read_line(&mut buffer).unwrap();
     let mut buffer = buffer.split_ascii_whitespace();
 
     let n = buffer.next().unwrap().parse::<usize>().unwrap();
     let m = buffer.next().unwrap().parse::<usize>().unwrap();
 
     let mut matrix: Vec<Vec<char>> = Vec::new();
-    let mut matrix: Vec<Vec<usize>> = vec![vec![0; n]; m];
-    let start = [0, 0];
-    let find_start = false;
+    let mut matrix2: Vec<Vec<usize>> = vec![vec![0; n]; m];
+    let mut start = [0, 0];
+    let mut find_start = false;
 
     for i in 0..n {
-        let line = buffer.next().unwrap();
-        let v : Vec<char> = line.chars().collect();
+        let mut line = String::new();
+        stdin().read_line(&mut line).unwrap();
+
+        let mut v : Vec<char> = line.chars().collect();
+        v.pop();
+
         if find_start == false {
             if let Some(position) = v.iter().position(|&x| x == 'I') {
                 find_start = true;
+                start[0] = i;
+                start[1] = position;
             }
         }
-        
-        
-
 
         matrix.push(v);
+
         
+
     }
+    // let start = [0, 0];
+    // let mut find_start = false;
+
+    // for i in 0..n {
+    //     let line = buffer.next().unwrap();
+    //     // let v : Vec<char> = line.chars().collect();
+    //     // if find_start == false {
+    //     //     if let Some(position) = v.iter().position(|&x| x == 'I') {
+    //     //         find_start = true;
+    //     //     }
+    //     // }
+    //     println!("{:?}", line);
+    //     // matrix.push(v);
+        
+    // }
 
     println!("{:?}", matrix);
+    println!("{:?}", start);
 
 
     
