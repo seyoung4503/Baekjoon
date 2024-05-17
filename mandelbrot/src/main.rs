4855fc460e2a6553e9d6f8323225ee758bf94cb1
seyoung4503 <seyoung4503@gmail.com>
2024-05-17 12:17:44 +0900

    add mandelbrot example

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use num::Complex;
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex {re: 0.0, im: 0.0};
+    loop {
+        z = z * z + c;
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex {re: 0.0, im: 0.0};
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+
+    None
 }
